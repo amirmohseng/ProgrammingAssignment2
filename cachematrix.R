4ad5bf84707dbf3ca45f27308aba0a8cbf958415
amirmohseng <a.golmohammady@gmail.com>
2016-10-03 12:48:06 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
 ## The function gets a matrix and return its invert
+## creates an R object that stores a vector and its mean 
 
 
-## Write a short comment describing this function
+
 
 makeVector <- function(x = matrix()) {
         m <- NULL
@@ -10,21 +11,25 @@ makeVector <- function(x = matrix()) {
                 m <<- NULL
         }
         get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
         list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
+                
+## cachmean calculates the inverse of the mtrix x
+## If it already exists in cache, it will report "getting cached data" and print the inverse
+## Otherwise the inverse is reported 
 
 cachemean <- function(x, ...) {
-        m <- x$getmean()
+        m <- x$getinverse()
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
         data <- x$get()
         m <- solve(data, ...)
-        x$setmean(m)
+        x$setinverse(m)
         m
 }
